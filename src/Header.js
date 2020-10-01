5afc773f8f6abd810e5b9142013e2f08fec27b3a
Thomas <t.garrison333@gmail.com>
2020-10-01 09:56:21 -0400

    small redundent paragraph change

--- src/Header.js
+++ src/Header.js
@@ -9,13 +9,13 @@ class Header extends Component {
               <div className="headerContainer">
                 <p>
                   Push the "Transcribe" button (found at the bottom of each
-                  spell card found in the compendium) found at the bottom of the
-                  spell cards to add a spell to your personal spellbook.
+                  spell card found in the compendium) to add a spell to your
+                  personal spellbook.
                 </p>
                 <p>
-                  In your spell book you may push the "Remove" (found at the
-                  bottom of each spell card found in Your Spellbook) button to
-                  take a spell out of your spellbook
+                  In your spell book you may push the "Remove" button (found at
+                  the bottom of each spell card found in Your Spellbook) to take
+                  a spell out of your spellbook
                 </p>
               </div>
             </header>
