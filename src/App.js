0fc9192d696d8db5aaaa24206eb7d2b2d94fbcbf
thomasDotOnline <t.garrison333@gmail.com>
2020-10-13 20:32:26 -0400

    name change for portfolio

--- src/App.js
+++ src/App.js
@@ -62,8 +62,8 @@ class App extends Component {
       .signInAnonymously()
       .catch(function (error) {
         // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
+        let errorCode = error.code;
+        let errorMessage = error.message;
         // ...
         console.log(errorCode);
         console.log(errorMessage);
@@ -72,8 +72,8 @@ class App extends Component {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         // User is signed in.
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
+        let isAnonymous = user.isAnonymous;
+        let uid = user.uid;
 
         this.setState({
           userId: uid,
